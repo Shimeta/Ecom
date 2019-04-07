c804440e103c1629902a1c2c92a84f616442a38f
shimeta <kahilrazor@gmail.com>
2019-04-07 22:52:43 +0200

    Navbar added

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h3>this is a Product</h3>
+      </div>
+    );
+  }
+}
